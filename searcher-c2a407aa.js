444851497e50056cb5cd0a9bd2ce7460ed1e793a
ZXY595 <112233374+ZXY595@users.noreply.github.com>
2025-10-28 13:00:07 +0000

    Deploying to gh-pages from @ mechanical-ancestor/team-training@0204bfa8d3135b14d87180b9db2d1c46f1abfb12 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-db64dc2c.js',
+                path_to_root + 'searchindex-243881d9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
